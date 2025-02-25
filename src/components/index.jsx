5ed4728d14ed90d82d9d5c7f42bed92981f445f0
alyradwan <ali011656@eng.zu.edu.eg>
2025-02-25 15:55:34 +0200

    rename SparkLine

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,3 +7,4 @@ export { default as LineChart } from "./Charts/LineChart";
 export { default as Stacked } from "./Charts/Stacked";
 export { default as Pie } from "./Charts/Pie";
 export { default as Header } from "./Header";
+// test
